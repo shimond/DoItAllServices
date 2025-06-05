4cce09c759f520c6358c3356afa2eb2ccce825f8
Shimon Dahan <shimond@any-techs.co.il>
2025-06-05 09:38:34 +0300

    Add request logging middleware

---
+++ CatalogApi/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,41 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace CatalogApi.Middleware;
+
+public class LoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<LoggingMiddleware> _logger;
+
+    public LoggingMiddleware(RequestDelegate next, ILogger<LoggingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (HttpMethods.IsPost(context.Request.Method))
+        {
+            context.Request.EnableBuffering();
+            using var reader = new StreamReader(context.Request.Body, leaveOpen: true);
+            var body = await reader.ReadToEndAsync();
+            context.Request.Body.Position = 0;
+            _logger.LogInformation("POST {Path} Body: {Body}", context.Request.Path, body);
+        }
+
+        await _next(context);
+    }
+}
+
+public static class LoggingMiddlewareExtensions
+{
+    public static IApplicationBuilder UseLoggingMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<LoggingMiddleware>();
+    }
+}
